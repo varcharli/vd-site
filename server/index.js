bd601f26c6ae9bb3f19b62cae0fc04cdc8ed816a
varcharli <varchar@qq.com>
2024-09-13 21:14:55 +0800

    rest start

--- server/index.js
+++ server/index.js
@@ -4,17 +4,20 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-// 路由
+//add movie routes at /routes/movieRoutes.js
+
+const movieRoutes = require('./routes/movieRoutes');
+router.use('/movies', movieRoutes.routes());
+
 router.get('/', async (ctx) => {
-  ctx.body = {
-    title: '首页',
-    message: '欢迎来到Koa首页!'
-  };
+  await ctx.render('index', { title: '首页', message: '欢迎来到Koa首页!' });
 });
 
-app
-  .use(router.routes())
+app.use(router.routes())
+  .use(movieRoutes.routes())
   .use(router.allowedMethods());
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
