5b5308955185b4e6c4b466f8481edf170ac2d372
varcharli <varchar@qq.com>
2024-11-05 13:01:23 +0800

    fixed myImage

--- client/videos-app/src/components/MyImage.js
+++ client/videos-app/src/components/MyImage.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import defaultImage from '../assets/null_movie.png'; // 引入默认图片
 
-export const MyImage = ({ src, alt, onLoad }) => {
+export const MyImage = ({ src, alt, onLoad, width, height }) => {
     const [isLoading, setIsLoading] = React.useState(true);
+    const toWidth = width || 100;
+    const toHeight = height || 200;
 
     const handleLoaded = () => {
         setIsLoading(false);
         if (onLoad) {
             onLoad();
         }
-    }   
+    }
 
     return (
         <div>
             {isLoading
-                ? <img src={defaultImage} alt="loading" />
+                ? <div style={{ width: toWidth, height: toHeight, backgroundColor: 'rgba(255, 255, 255, 0.2)' }}></div>
                 : null
             }
             <img
+                style={isLoading ? { display: 'none' } : {}}
                 src={src || defaultImage}
                 alt={alt}
                 onLoad={handleLoaded}
